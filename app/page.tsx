37318d350cb66e612004067e09468be1c88ffa12
Ryan varela <108085059+varelaprogramador@users.noreply.github.com>
2024-08-05 17:11:10 -0300

    commit quality

--- app/page.tsx
+++ app/page.tsx
@@ -15,97 +15,106 @@ const sora = Sora({
 
 export default function Home() {
   return (
-    <main className={`flex min-h-screen flex-col items-center bg-white justify-between text-white ${sora.className}`}>
-      <section className="w-full h-[500px] flex flex-wrap bg-cover bg-center"
+    <main className={`flex min-h-screen flex-col items-center bg-black justify-between text-white ${sora.className}`}>
+      <section className=" pb-[40px] w-full min-h-[500px] flex flex-wrap bg-cover bg-center p-12"
         style={{ backgroundImage: `url(${bannerMain.src})` }}>
           <div className=" w-[50%] flex flex-col gap-4 justify-center pl-40 max-sm:w-full max-sm:pl-7 ">
           <Image src={logoyt}
+          quality={100}
          alt="logo"
          className="w-[160px] h-[110px]"
           ></Image>
-          <h1 className=" text-[30px]">
-            Vale a pena <span className="text-cyan-400">larga seu emprego para </span><span className="bg-red-600 text-white font-bold p-1">SER YOUTUBER?</span>
+          <h1 className=" text-[3rem] max-lg:text-[2rem] ">
+            Vale a pena <span className="text-cyan-400">larga seu emprego  </span> para<br /><span className="bg-red-600 text-white font-bold p-1">SER YOUTUBER?</span>
           </h1>
-          <p className=" text-sm">
+          <p className=" text-[1rem] text-gray-300 min-[1200px]:w-[90%]">
             Um curso completo para você começar do zero e se 
             tornar um Youtuber de sucesso!
           </p>
           <Link href={"#"}>
-          <button className="bg-cyan-400 rounded-md p-1">
-            Quero aprender
+          <button className="bg-cyan-400 text-2xl text-white font-black rounded-md p-2">
+            quero aprender
           </button>
           </Link>
           
           </div>
-          <div className=" w-[50%] max-sm:hidden">
+          <div className=" w-[50%] flex items-end max-sm:hidden ">
+            <div className="w-full">
             <Image
+            quality={100}
+            className="w-full "
             src={fotoThiago}
             alt="foto do thiago"
             ></Image>
+            </div>
 
           </div>
       </section>
-      <section className="w-full min-h-[500px] flex bg-cover bg-center"
+      <section className=" pb-[40px] pt-5 w-full min-h-[500px] flex bg-cover bg-center"
         style={{ backgroundImage: `url(${bannerMain2.src})` }}>
           <div className=" w-full flex flex-col gap-4 items-center ">
           
-          <h2 className=" text-[30px] text-center pt-16">
+          <h2 className=" text-[3rem] max-sm:text-[2rem]  text-center pt-16">
             Um curso para <br /> <span className=" font-bold">você aprender do zero! </span>
           </h2>
        
           <Link href={"#"}>
-          <button className="bg-cyan-400 rounded-md p-1">
-            entrar no grupo 
+          <button className="bg-cyan-400 text-2xl text-white font-black rounded-md p-2">
+          entrar para o grupo
           </button>
           </Link>
           
           </div>
           
       </section>
-      <section className="w-full min-h-[500px] flex bg-black"
+      <section className=" pb-[150px] pt-5 w-full min-h-[500px] flex bg-black"
         >
-          <div className=" w-full flex flex-col gap-8 items-center ">
+          <div className=" w-full flex flex-col gap-[60px] items-center ">
           
-          <h2 className=" text-[30px] text-center pt-16">
+          <h2 className=" text-[3rem] max-sm:text-[2rem] text-center pt-16">
             O que você <br /> <span className=" font-bold">vai aprender? </span>
           </h2>
-
+          
           <Image
           src={carousel}
+          quality={100}
           className="w-full bg-cover"
           alt="carrousel-aulas"
           ></Image>
+         
        
-          <Link href={"#"}>
-          <button className="bg-cyan-400 rounded-md p-1">
-            entrar no grupo 
+         <Link href={"#"}>
+          <button className="bg-cyan-400 text-2xl text-white font-black rounded-md p-2">
+          entrar para o grupo
           </button>
           </Link>
           
           </div>
           
       </section>
-      <section className="w-full min-h-[500px] flex flex-wrap bg-black">
-          <div className=" w-[50%] flex flex-col gap-4 justify-center pl-40 max-sm:w-full max-sm:p-8">
+      <section className="min-[1000px]:pb-[150px] pt-5 w-full min-h-[500px] flex flex-wrap bg-black">
+          <div className=" w-[50%] flex flex-col gap-4 justify-center pl-40 max-lg:w-full max-lg:p-8">
           <Image src={logoyt}
+          quality={100}
          alt="logo"
          className="w-[160px] h-[110px]"
           ></Image>
-          <h2 className=" text-[30px]">
+          <h2 className=" text-[3rem] max-sm:text-[2rem] ">
            <span className="text-cyan-400">Ficou Interessado? <br /></span>Entre no <span className=" text-white font-bold p-1">grupo VIP</span> para garantir sua vaga em primeira mão
           </h2>
           
           <Link href={"#"}>
-          <button className="bg-cyan-400 rounded-md p-1">
-            Quero aprender
+          <button className="bg-cyan-400 text-2xl text-white font-black rounded-md p-2">
+          entrar para o grupo
           </button>
           </Link>
           
           </div>
-          <div className=" max-sm:w-full  flex justify-end  bg-cover bg-right"
+          <div className=" max-lg:w-full  flex justify-end  bg-cover bg-right"
           
           style={{ backgroundImage: `url(${fundoCelular.src})` }}>
             <Image
+            quality={100}
             src={celular}
             className="w-[350px] max-sm:w-[250px] "
             alt="celular"
@@ -113,23 +122,23 @@ export default function Home() {
 
           </div>
       </section>
-      <section className=" min-h-[500px] flex   bg-cyan-400 p-24 text-black">
-        <div className="flex flex-wrap min-h-[500px]  border border-black rounded-md p-10">
-          <div className="w-[50%] flex  justify-center items-center p-1 max-sm:w-full">
-            <h2 className=" text-[40px]">
+      <section className=" pb-[40px]pt-5  w-full min-h-[500px] flex justify-center bg-cyan-400 p-24 max-sm:p-10 text-black">
+        <div className="flex flex-row max-lg:flex-col   h-fit border border-black rounded-md p-24 gap-[50px] max-sm:p-6">
+          <div className="w-[50%] flex  justify-center items-center p-1 max-lg:w-full">
+            <h2 className=" text-[3rem] max-sm:text-[2rem]  ">
               Faltam poucos dias para <span className="bg-red-600  font-bold p-1">abrirem as vagas</span>
             </h2>
 
           </div>
-          <div className="w-[50%]  flex flex-wrap justify-center items-center max-sm:w-auto p-1">
-            <div className="bg-white flex items-center justify-center w-[30%] h-[40%] max-sm:w-auto max-sm:p-3 max-sm:h-auto rounded-md">
-              <h2 className="text-[80px] max-sm:text-[25px] font-extrabold">10</h2>
+          <div className="w-[50%] gap-5  flex  justify-center items-center max-lg:w-full max-sm:h-fit p-1">
+            <div className="bg-white flex items-center justify-center p-11 max-sm:w-auto max-sm:p-[15px] max-sm:h-auto  rounded-md">
+              <h2 className="text-[80px] max-lg:text-[60px] max-sm:text-[55px] font-extrabold">10</h2>
             </div>
-            <h2 className="text-[80px] max-sm:text-[20px] max-sm:h-auto font-extrabold p-6">
+            <h2 className="text-[80px] max-lg:text-[60px] max-sm:text-[55px] max-sm:h-auto font-extrabold p-6">
               :
             </h2>
-            <div className="bg-white w-[30%] flex items-center justify-center  h-[40%] max-sm:w-auto max-sm:p-3 max-sm:h-auto  rounded-md">
-            <h2 className="text-[80px] max-sm:text-[25px] font-extrabold">D</h2>
+            <div className="bg-white p-11 flex items-center justify-center   max-sm:w-auto max-sm:p-[15px] max-sm:h-auto  rounded-md">
+            <h2 className="text-[80px] max-lg:text-[60px] max-sm:text-[55px] font-extrabold">D</h2>
             </div>
 
           </div>
