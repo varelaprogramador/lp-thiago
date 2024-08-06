6e8d122efb67c80d7b9f9d08713eb31491783321
Ryan varela <108085059+varelaprogramador@users.noreply.github.com>
2024-08-06 10:49:16 -0300

    bg-att

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ console.log(`quantidade de dias restantes ${days}`);
 
 export default function Home() {
   return (
-    <main className={`flex min-h-screen flex-col items-center bg-gray-900 justify-between text-white ${sora.className}`}>
+    <main className={`flex min-h-screen flex-col items-center bg-[#0d0d0d] justify-between text-white ${sora.className}`}>
       <section className=" pb-[40px] w-full min-h-[500px] flex flex-wrap bg-cover bg-center p-12"
         style={{ backgroundImage: `url(${bannerMain.src})` }}>
         <div className=" w-[50%] flex flex-col gap-4 justify-center   pl-[15%] max-sm:w-full max-sm:pl-7 ">
