45c74cdc21b19bf4cf3c534c06e83778a6b263e2
Bruno Zielinski De Medeiros <brunozie26@gmail.com>
2024-08-14 01:31:18 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
                 openCreateSpotLead({
                   eventId: userIdentifier,
                   whatsappGroup:
-                    'https://chat.whatsapp.com/FnqqWr7JIf5G2WRCrRg0Sr',
+                    'https://chat.whatsapp.com/CDl0LHfAcz0EeJRYhLFVWj',
                 })
               }}
             >
@@ -140,7 +140,7 @@ export default function Home() {
                 openCreateSpotLead({
                   eventId: userIdentifier,
                   whatsappGroup:
-                    'https://chat.whatsapp.com/FnqqWr7JIf5G2WRCrRg0Sr',
+                    'https://chat.whatsapp.com/CDl0LHfAcz0EeJRYhLFVWj',
                 })
               }}
             >
@@ -171,7 +171,7 @@ export default function Home() {
                 openCreateSpotLead({
                   eventId: userIdentifier,
                   whatsappGroup:
-                    'https://chat.whatsapp.com/FnqqWr7JIf5G2WRCrRg0Sr',
+                    'https://chat.whatsapp.com/CDl0LHfAcz0EeJRYhLFVWj',
                 })
               }}
             >
@@ -206,7 +206,7 @@ export default function Home() {
                 openCreateSpotLead({
                   eventId: userIdentifier,
                   whatsappGroup:
-                    'https://chat.whatsapp.com/FnqqWr7JIf5G2WRCrRg0Sr',
+                    'https://chat.whatsapp.com/CDl0LHfAcz0EeJRYhLFVWj',
                 })
               }}
             >
